586eea3976614209826699a63e3827b92137c921
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-11-01 10:36:24 +0000

    added clear function for display

--- javascript.js
+++ javascript.js
@@ -14,35 +14,45 @@ function divide(a,b) {
     return a / b;
 }
 
-let operand1
-let operand2
-let operator
+let operand1;
+let operand2;
+let operator;
 
 function operate(a,b,c) {
     if (c == '+') {
-        return add(a,b)
+        return add(a,b);
     } else if (c == '-') {
-        return subtract(a,b)
+        return subtract(a,b);
     } else if (c == '*') {
-        return multiply(a,b)
+        return multiply(a,b);
     } else if (c == '/') {
-        return divide(a,b)
+        return divide(a,b);
     }
 }
 
-const digits = document.querySelectorAll('.digit')
-const display = document.querySelector('.display')
-const operatorButton = document.querySelectorAll('.operator')
+const digits = document.querySelectorAll('.digit');
+const display = document.querySelector('.display');
+const operatorButton = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear')
+
+clearButton.addEventListener('click', () => {
+    display.textContent = ''
+})
 
 digits.forEach((digit) => {
     digit.addEventListener('click', () => {
-        display.textContent = display.textContent + digit.textContent
-        operand1 = parseFloat(display.textContent)
+        display.textContent = display.textContent + digit.textContent;
     })
 })
 
 operatorButton.forEach((operatorSign) => {
     operatorSign.addEventListener('click', () => {
-        operator = operatorSign.textContent
+        operator = operatorSign.textContent;
+        if (operand1 === undefined) {
+            operand1 = parseFloat(display.textContent)
+        } else {
+            operand2 = parseFloat(display.textContent)
+        }
+
     })
 })
