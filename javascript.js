73b8f29c34035b0991c8f23d79ab5b2f42acfaf5
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-11-01 05:26:38 +0000

    added basic math functions + tests in console

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+
+console.log(add(3,6));
+console.log(subtract(9,7));
+console.log(multiply(8,4));
+console.log(divide(72,8));
