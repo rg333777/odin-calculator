611181a287cd02992520f14ad87558b7df862d6c
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-11-01 05:35:46 +0000

    added operate function

--- javascript.js
+++ javascript.js
@@ -18,6 +18,18 @@ let operand1
 let operand2
 let operator
 
+function operate(a,b,c) {
+    if (c == '+') {
+        return add(a,b)
+    } else if (c == '-') {
+        return subtract(a,b)
+    } else if (c == '*') {
+        return multiply(a,b)
+    } else if (c == '/') {
+        return divide(a,b)
+    }
+}
+
 console.log(add(3,6));
 console.log(subtract(9,7));
 console.log(multiply(8,4));
